7a64fea09241c43912cf4e6fbf7b806e8047b294
LuisLozano0144 <82198290+LuisLozano0144@users.noreply.github.com>
2023-05-11 23:24:05 -0500

    se agrega endpint

--- src/logic/shops/data/shops.models.ts
+++ src/logic/shops/data/shops.models.ts
@@ -1,7 +1,19 @@
 export interface ResponseShops {
-  id: string | number;
-  name: string;
+  Address: string;
+  Email: string;
+  Facebook: string;
+  Id_sellers: number;
+  Id_stores: number;
+  Instagram: string;
   logo: string;
+  Num_telephone: string | number;
+  Page_web: string;
+  active_bank_account_number: number;
+  business_description: string;
+  keyword: string;
+  main_color: string;
+  name_store: string;
+  state: number;
 }
 
 export interface RequestMakeShop {
