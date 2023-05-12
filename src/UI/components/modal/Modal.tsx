9f601a9315f7b7cd54c56f75c757d0816c2c7e9e
JampierMoreno <67984118+jampier-santiago@users.noreply.github.com>
2023-05-12 16:42:45 -0500

    Fix endpoint stores

---
+++ src/UI/components/modal/Modal.tsx
@@ -0,0 +1,13 @@
+// Packages
+import { FC } from "react";
+
+import useStyles from "./modal.styles";
+
+const Modal: FC = () => {
+  // Styles
+  const {} = useStyles();
+
+  return <section></section>;
+};
+
+export default Modal;
---
+++ src/UI/components/modal/modal.styles.ts
@@ -0,0 +1,12 @@
+import tw from "twin.macro";
+import styled from "styled-components";
+
+const StyledContainer = styled.section`
+  ${tw``}
+`;
+
+const useStyles = () => {
+  return {};
+};
+
+export default useStyles;
