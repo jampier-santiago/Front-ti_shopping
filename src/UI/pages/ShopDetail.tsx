f93cc3a96336614c16df8c0f7beee74b07b74653
JampierMoreno <67984118+jampier-santiago@users.noreply.github.com>
2023-05-20 00:42:23 -0500

    Fix url

--- src/UI/pages/ShopDetail.tsx
+++ src/UI/pages/ShopDetail.tsx
@@ -2,10 +2,10 @@ import useViews from "..";
 import useStyles from "styles";
 
 //ASSETS
-import logo from "../../../assets/img/Logo_TI_Shopping.png";
-import iconFacebook from "../../assets/icons/Logo-facebook.png";
-import iconTwitter from "../../assets/icons/Logo-twitter.png";
-import iconInstagram from "../../assets/icons/Logo-instagram.png";
+import logo from "assets/img/Logo_TI_Shopping.png";
+import iconFacebook from "assets/icons/Logo-facebook.png";
+import iconTwitter from "assets/icons/Logo-twitter.png";
+import iconInstagram from "assets/icons/Logo-instagram.png";
 
 //controller
 import useShopDetailApplications from "logic/shopsDetail/application/shopsDetail.application";
@@ -46,10 +46,10 @@ const ShopDetail = () => {
           <StyleSubTittle>{stores?.Page_web}</StyleSubTittle>
           <StyleSubTittle>{stores?.Instagram}</StyleSubTittle>
           <StyledIcon>
-            <a target="_blank" href={stores?.Facebook}>
+            <a target="_blank" href={stores?.Facebook} rel="noreferrer">
               <img src={iconFacebook} alt="Logo-Facebook" />
             </a>
-            <a target="_blank" href={stores?.Instagram}>
+            <a target="_blank" href={stores?.Instagram} rel="noreferrer">
               <img src={iconInstagram} alt="Logo-instagram" />
             </a>
           </StyledIcon>
